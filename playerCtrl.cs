8eef83704ee5d45f0719c0d7713f3955c8da5f75
big616 <big616@users.noreply.github.com>
2018-03-31 15:39:06 +0800

    Add files via upload

---
+++ playerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCtrl : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	private int count;
+	void Start ()
+	{ 
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+	}
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float moveHoricontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHoricontal, 0.0f, moveVertical); 
+
+		rb.AddForce (movement * speed); 
+	}
+	
+		void OnTriggerEnter(Collider other) 
+		{
+		if (other.gameObject.CompareTag ("pickup")) 
+		{
+			other.gameObject.SetActive (false);
+		}
+		
+	}
+}
